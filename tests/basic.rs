bba8f2db6d21dfabcd0a0737455ae31f04a966c7
Dan Gohman <dev@sunfishcode.online>
2021-12-24 10:15:04 -0800

    Rename variables to fix a clippy warning.

--- tests/basic.rs
+++ tests/basic.rs
@@ -3,10 +3,11 @@ use open_ambient::{open_ambient_dir, open_ambient_file, open_ambient_file_with};
 
 #[test]
 fn basic() {
-    let _err = open_ambient_file!("lmot.ograC").unwrap_err();
+    let _file_err = open_ambient_file!("lmot.ograC").unwrap_err();
     let _file = open_ambient_file!("Cargo.toml").unwrap();
-    let _err = open_ambient_file_with!("lmot.ograC", OpenOptions::new().read(true)).unwrap_err();
-    let _file = open_ambient_file_with!("Cargo.toml", OpenOptions::new().read(true)).unwrap();
-    let _err = open_ambient_dir!("Cargo.toml").unwrap_err();
+    let _file_with_err =
+        open_ambient_file_with!("lmot.ograC", OpenOptions::new().read(true)).unwrap_err();
+    let _file_with = open_ambient_file_with!("Cargo.toml", OpenOptions::new().read(true)).unwrap();
+    let _dir_err = open_ambient_dir!("Cargo.toml").unwrap_err();
     let _dir = open_ambient_dir!(".").unwrap();
 }
